f57df70e07d40af102c380b373f6df68d2da48d7
Tolani99 <tolaniadisa999@gmail.com>
2023-11-28 17:34:48 +0100

    Using merge sort to sort an array in ascending order

--- algorithms/merge_sort/merge_sort_implementation_in_python.py
+++ algorithms/merge_sort/merge_sort_implementation_in_python.py
@@ -114,7 +114,7 @@ def printList(arr):
 
 # Driver Code
 if __name__ == '__main__':
-	arr = [12, 11, 13, 5, 6, 7]
+	arr = [3, 12, 10, 5, 6, 7]
 	print("Given array is")
 	printList(arr)
 	mergeSort(arr)
