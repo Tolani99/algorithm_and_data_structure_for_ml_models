94bf828266722e9eb547dc8dd8c3166b92890293
Tolani99 <tolaniadisa999@gmail.com>
2023-12-04 08:04:49 +0100

    changed data zillow name

---
+++ projects/zillow_home_value.py
@@ -0,0 +1,16 @@
+# Importing necessary libraries
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sb 
+from sklearn.model_selection import train_test_split 
+from sklearn.preprocessing import LabelEncoder, StandardScaler 
+from sklearn import metrics 
+from sklearn.svm import SVC 
+from xgboost import XGBRegressor 
+from sklearn.linear_model import LinearRegression, Lasso, Ridge 
+from sklearn.ensemble import RandomForestRegressor 
+
+# Suppressing warnings to improve code readability
+import warnings 
+warnings.filterwarnings('ignore')
